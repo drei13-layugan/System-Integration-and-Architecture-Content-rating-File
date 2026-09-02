9de15207cacb8ad58cefc6ca8c1910cccb6e2cf3
Andrei Roland Layugan <andreirolandlayugan@gmail.com>
2026-09-02 14:49:25 +0800

    first commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,39 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes:0,
+      disLikes:0,
+      handlelike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+
+      },
+
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          disLikes: prevState.disLikes + 1
+        }));
+      }
+    }
   }
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <div className="content-rating">
+       <p>
+        //Add text here
+        </p>
+         <div className="rating-buttons">
+           <button className="like-button" onClick={this.state.handlelike}>
+            Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.disLikes})
+            </button>
+          </div>      
+     </div >
     );
   }
 }
